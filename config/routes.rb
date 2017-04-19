29b0e09790bde7b18055a527461640225bfc2e61
Buck Rivard <buckrivard@gmail.com>
2017-04-19 11:57:42 -0700

    user profile added

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,14 @@ Rails.application.routes.draw do
 
   get 'home/index'
 
+	get 'profile', to: 'users#show'
+  
   devise_for :users
-  root 'home#index'
+
+  authenticated :user do
+  	root 'users#show', as: :authenticated_root
+  end
+
+  root to: 'home#index'
 
 end
